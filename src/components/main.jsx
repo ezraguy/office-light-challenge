cdba6ddf29eb4d8a1b8bbeddf992987607b706d7
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-11-06 15:03:53 +0200

    adding first responsive section

---
+++ src/components/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from '../images/share/logo.svg';
+import headerBackground from '../images/home/bg-pattern-header.svg'
+import mainPattern from '../images/home/illustration-charts.svg'
+import '../scss/main.scss';
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="header">
+                <div className="logo">
+                    <img src={logo} alt="" />
+
+                </div>
+            </div>
+
+            <img src={headerBackground} className="headerBackgroundImg" alt="" />
+
+            <div className="main-wrap">
+
+
+                <div className="main-content">
+                    <div >
+                        <p className="main-title">A simple solution to complex tasks is coming soon</p>
+                    </div>
+                    <div >
+                        <p className="main-text"> Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new collaboration platform built with an intuitive interface to improve productivity. </p>
+                    </div>
+                    <button className="ui button getStarted">Get started</button>
+                </div>
+                <div className="main-img-wrap">
+                    <img src={mainPattern} className="main-img" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;

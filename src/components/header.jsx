e63315308c82e7005a9e4604fa8e2fe74202b51f
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-11-19 13:59:17 +0200

    making logo a link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../images/share/logo.svg';
 import '../scss/header.scss'
 const Header = () => {
     return (<div className="header">
         <div className="logo">
-            <img src={logo} alt="" />
+            <Link to="/">
+                <img src={logo} alt="logo" />
+            </Link>
         </div>
     </div>);
 }
